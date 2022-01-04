0bf409eb1ba5b8a592a951e352916020efaed677
yk <re703729@gmail.com>
2022-01-04 14:27:28 +0800

    -

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/test", methods=['GET'])
+@app.route("/test", methods=['POST'])
 def test():
     res = json.loads(request.data.decode('utf-8'))
     return res
