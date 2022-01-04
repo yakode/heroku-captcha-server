eccc087086d72e215ee138ed6b7071999b4fd1f7
yk <re703729@gmail.com>
2022-01-04 12:40:01 +0800

    comment the imprted package

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-from flask import request, Flask,jsonify
-import torch
-from PIL import Image
-import torchvision.transforms as transforms
-from torch.autograd import Variable
-from pytorch_model import CNN
-import json
-from io import BytesIO
-from base64 import b64decode
+# from flask import request, Flask,jsonify
+# import torch
+# from PIL import Image
+# import torchvision.transforms as transforms
+# from torch.autograd import Variable
+# from pytorch_model import CNN
+# import json
+# from io import BytesIO
+# from base64 import b64decode
 
 app = Flask(__name__)
 
