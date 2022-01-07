77d12a73aa641d39d9d7410a4d46c28e01c6f75f
yk <re703729@gmail.com>
2022-01-07 10:16:54 +0800

    clock

--- clock.py
+++ clock.py
@@ -9,6 +9,7 @@ def scheduled_job():
     url = "https://eeclass-captcha.herokuapp.com/"
     conn = urllib.urlopen(url)
     now = time.gmtime(1569376996)
+    print(now)
     if now.tm_hour == 2:
         if now.tm_min >= 0:
             url = "https://elearn-captcha.herokuapp.com/"
