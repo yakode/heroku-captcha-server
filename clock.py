e5cde93bf7d8b3d68b2425fb3afec020f5848449
yk <re703729@gmail.com>
2022-01-07 10:33:43 +0800

    clock

--- clock.py
+++ clock.py
@@ -4,13 +4,17 @@ import time
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('cron', hour='0-12', minute='*/2')
+@sched.scheduled_job('cron', hour='0-12', minute='*/1')
 def scheduled_job():
     url = "https://eeclass-captcha.herokuapp.com/"
     conn = urllib.urlopen(url)
     now = time.gmtime(time.time())
+    print("=================================")
+    print(now)
+    print("=================================")
     if now.tm_hour == 2:
         if now.tm_min >= 0:
+            print("ya")
             url = "https://elearn-captcha.herokuapp.com/"
             conn = urllib.urlopen(url)
 
