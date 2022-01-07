37260a2b5787a1f31239b6a5636f7bccac3067aa
yk <re703729@gmail.com>
2022-01-07 11:09:08 +0800

    clockB

--- clock.py
+++ clock.py
@@ -4,16 +4,16 @@ import time
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('cron', hour='0-11', minute='*/15')
+@sched.scheduled_job('cron', hour='12-23', minute='*/15')
 def scheduled_job():
     now = time.gmtime(time.time())
-    if now.tm_hour == 11  and now.tm_min >= 45:
+    if now.tm_hour == 23  and now.tm_min >= 45:
         print("wake up the other app")
-        url = "https://elearn-captcha.herokuapp.com/"
+        url = "https://eeclass-captcha.herokuapp.com/"
         conn = urllib.urlopen(url)
     else:
         print("wake up this app")
-        url = "https://eeclass-captcha.herokuapp.com/"
+        url = "https://elearn-captcha.herokuapp.com/"
         conn = urllib.urlopen(url)
 
 sched.start()
