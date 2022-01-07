0b54cc8d7a193372b3a06e7e2a4547c0507ac071
yk <re703729@gmail.com>
2022-01-07 09:22:55 +0800

    clock

--- clock.py
+++ clock.py
@@ -4,12 +4,12 @@ import urllib.request as urllib
 sched_self = BlockingScheduler()
 sched_other = BlockingScheduler()
 
-@sched_self.scheduled_job_self('cron', hour='0-12', minute='*/20')
+@sched_self.scheduled_job('cron', hour='0-12', minute='*/20')
 def scheduled_job_self():
     url = "https://eeclass-captcha.herokuapp.com/"
     conn = urllib.urlopen(url)
 
-@sched_other.scheduled_job_other('cron', hour='9', minute='25')
+@sched_other.scheduled_job('cron', hour='9', minute='25')
 def scheduled_job_other():
     url = "https://elearn-captcha.herokuapp.com/"
     conn = urllib.urlopen(url)
