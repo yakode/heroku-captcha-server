f9f3f867103e07e090f5563f939c5dae950ff34d
yk <re703729@gmail.com>
2022-01-07 10:03:39 +0800

    clock

--- clock.py
+++ clock.py
@@ -9,8 +9,8 @@ def scheduled_job():
     url = "https://eeclass-captcha.herokuapp.com/"
     conn = urllib.urlopen(url)
     now = time.gmtime(1569376996)
-    if now.tm_hour == 1:
-        if now.tm_min >= 39:
+    if now.tm_hour == 2:
+        if now.tm_min >= 0:
             url = "https://elearn-captcha.herokuapp.com/"
             conn = urllib.urlopen(url)
 
