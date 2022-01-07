4e948eb29b62d34a4814c046ee98ab6f76878b08
yk <re703729@gmail.com>
2022-01-07 08:54:32 +0800

    clock

--- clock.py
+++ clock.py
@@ -1,13 +1,12 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
+import urllib
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('cron', hour='0-12', minute='*/29')
+@sched.scheduled_job('cron', hour='0-9', minute='*/2')
 def scheduled_job():
     url = "https://eeclass-captcha.herokuapp.com/"
     conn = urllib.request.urlopen(url)
-        
-    # for key, value in conn.getheaders():
-    #     print(key, value)
+
 
 sched.start()
